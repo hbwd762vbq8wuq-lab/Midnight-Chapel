6a560f5975b4984e43d5a8e78575def8c4601c74
hbwd762vbq8wuq-lab <hbwd762vbq.8w.uq@gmail.com>
2026-06-06 11:30:04 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The Priest forged in darkness, forged by the hands of @roxe_.s",
+        name: "The Priest forged in darkness, forged by the hands of the goat soarch",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 4, 
       },
     ],
   },
